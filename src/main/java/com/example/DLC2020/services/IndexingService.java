fe91a2307ae355e609dc8294b1ee10b64b77adfd
notebook <pablofgarzon@gmail.com>
2020-06-03 18:06:45 -0300

    servicio de indexacion

--- src/main/java/com/example/DLC2020/services/IndexingService.java
+++ src/main/java/com/example/DLC2020/services/IndexingService.java
@@ -2,23 +2,37 @@ package com.example.DLC2020.services;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
 import com.example.DLC2020.Commons;
 import com.example.DLC2020.dal.commons.DocumentoDao;
+import com.example.DLC2020.dal.commons.VocabularioDao;
 import com.example.DLC2020.entities.Documento;
 import com.example.DLC2020.entities.Posteo;
 import com.example.DLC2020.entities.Vocabulario;
 
 public class IndexingService {
-	
-	DocumentoDao dd;
-	
+
+	private final DocumentoDao documentoDao;
+	private final VocabularioDao vocabularioDao;
+	private List<String> ignoredWords = new ArrayList<>();
+
 	@Inject
-	public IndexingService(DocumentoDao dd) {
-		this.dd = dd;
+	public IndexingService(DocumentoDao documentoDao, VocabularioDao vocabularioDao) {
+		this.documentoDao = documentoDao;
+		this.vocabularioDao = vocabularioDao;
 	}
 
 	public void indexing(String folder) throws Exception {
@@ -27,69 +41,79 @@ public class IndexingService {
 		if (!directory.isDirectory())
 			throw new Exception("not valid directory");
 
+		Map<String, Map<Integer, Integer>> wordPosts = new HashMap<>();
+		Map<Integer, Documento> docs = new HashMap<>();
+
 		for (final File file : directory.listFiles()) {
 			Documento d = new Documento();
 			d.setNombre(file.getName());
 			d.setUrl(folder + "/" + file.getName());
-			dd.create(d);
-			saveWords(file, d);
+			documentoDao.create(d);
+			readFile(file, d.getIddoc(), wordPosts);
+			docs.put(d.getIddoc(), d);
+		}
+		save(wordPosts, docs);
+	}
+
+	public void readFile(File file, int iddoc, Map<String, Map<Integer, Integer>> wordPosts) {
+
+		try (Stream<String> stream = Files.lines(file.toPath(), StandardCharsets.ISO_8859_1)) {
+			stream.forEach(l -> {
+				List<String> line = Stream.of(l.toLowerCase().split(Commons.delims)).collect(Collectors.toList());
+
+				// remover los stopwords
+				line.removeAll(Commons.STOP_WORDS);
+
+				for (String word : line) {
+					if (ignoredWords.contains(word))
+						continue;
+					Map<Integer, Integer> posts = wordPosts.getOrDefault(word, new HashMap());
+					int tf = posts.getOrDefault(iddoc, 0) + 1;
+					if (tf < 50) {
+						posts.put(iddoc, tf);
+						wordPosts.put(word, posts);
+					} else {
+						if (word.length() < 2) {
+							wordPosts.remove(word);
+							ignoredWords.add(word);
+						}
+					}
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	public void saveWords(File file, Documento doc) {
-		StringBuilder sb=new StringBuilder();
-        
-		try (Scanner sc = new Scanner(file)) {
-            while (sc.hasNextLine()) {
-                sb.append(sc.nextLine()).append("\n");
 
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("No existe el archivo de entrada...");
-        }
+	private void save(Map<String, Map<Integer, Integer>> wordPosts, Map<Integer, Documento> docs) {
+		List<Vocabulario> words = new ArrayList<>();
 		
-		String [] words = sb.toString().split(Commons.delims);
-		
-		for(int i =0; i< words.length;i++)
-        {
-            Vocabulario v =new Vocabulario(words[i].trim().toLowerCase());
-            
-//            ht.put(d.getIddoc(), palabras[i].trim().toLowerCase());
-//                        
-//            
-//            Posteo p=new Posteo(new PosteoPK(palabras[i].trim().toLowerCase(), d.getIddoc()));
-//            
-//            Posteo p = new Posteo();
-//			p.setVocabulario(v);
-//			p.setDocumento(doc);
-//			p.setTf(doc.getValue());
-//			vocabulario.addPosteo(p);
-//            
-//            
-//            p.setVocabulario(v);
-//            p.setDocumentos(d);
-//            PosteoDao pd=new PosteoDao();
-//            VocabularioDao vd= new VocabularioDao();
-//            try{
-//
-//                try{
-//                    vd.create(v);
-//                }
-//                catch(Exception e){
-//                    v=vd.retrieve(palabras[i].trim().toLowerCase());
-//                    vd.update(v);
-//                }
-//                pd.create(p);
-//            
-//            }catch(Exception e){
-//                //System.out.println("Sale por la excepcion: "+ e.getMessage());
-//                //System.out.println("Clase de excepcion: "+e.getClass());
-//                p=pd.retrieve(d.getIddoc(),v.getPalabra());
-//                //System.out.println(p.toString());
-//                p.aumentarTf(); //si el termino ya aparecio, incremento en 1
-//                pd.update(p);
-//            }
-                        
-        }
+		int cnt = 0;
+
+		for (Entry<String, Map<Integer, Integer>> entry : wordPosts.entrySet()) {
+			String word = entry.getKey();
+			Map<Integer, Integer> posts = entry.getValue();
+
+			Vocabulario v = new Vocabulario(word);
+
+			for (Entry<Integer, Integer> post : posts.entrySet()) {
+				int iddoc = post.getKey();
+
+				Posteo p = new Posteo();
+				p.setVocabulario(v);
+				p.setDocumento(docs.get(iddoc));
+				p.setTf(post.getValue());
+				v.addPosteo(p);
+			}
+			vocabularioDao.create(v);
+			//words.add(v);
+			
+//			if (cnt++ % 50 == 0) {
+//				vocabularioDao.createBatch(words);
+//				words.clear();
+//			}
+		}
+//		System.out.println(words.size());
 	}
 }
