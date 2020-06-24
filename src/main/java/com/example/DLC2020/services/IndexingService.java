918132779f7c494e1bedc476793c37db9696ef18
notebook <pablofgarzon@gmail.com>
2020-06-24 04:25:23 -0300

    se agregan stop words, se quita validaciones de palabras en service

--- src/main/java/com/example/DLC2020/services/IndexingService.java
+++ src/main/java/com/example/DLC2020/services/IndexingService.java
@@ -25,8 +25,6 @@ public class IndexingService {
 
 	private final DocumentoDao documentoDao;
 	private final VocabularioDao vocabularioDao;
-	//lista para palabras ignoradas
-	private List<String> ignoredWords = new ArrayList<>();
 
 	@Inject
 	public IndexingService(DocumentoDao documentoDao, VocabularioDao vocabularioDao) {
@@ -40,10 +38,10 @@ public class IndexingService {
 		if (!directory.isDirectory())
 			throw new Exception("not valid directory");
 
-		//map de primitivos donde la clave es la palabra, el valor seria otro map con 
+		// map de primitivos donde la clave es la palabra, el valor seria otro map con 
 		// el iddoc como clave y la cantidad de ocurrencias como valor
 		Map<String, Map<Integer, Integer>> wordPosts = new HashMap<>();
-		//map con iddoc y el documento
+		// map con iddoc y el documento
 		Map<Integer, Documento> docs = new HashMap<>();
 
 		int index = 0;
@@ -52,10 +50,10 @@ public class IndexingService {
 			d.setNombre(file.getName());
 			d.setUrl(folder + "/" + file.getName());
 			documentoDao.create(d);
-			docs.put(d.getIddoc(), d);
 			index += 1;
 			System.out.println("leyendo " + file.getName()+ " - " + index);
 			readFile(file, d.getIddoc(), wordPosts);
+			docs.put(d.getIddoc(), d);
 		}
 		save(wordPosts, docs);
 	}
@@ -69,23 +67,11 @@ public class IndexingService {
 				line.removeAll(Constants.STOP_WORDS);
 
 				for (String word : line) {
-					// si esta en el listado de palabras ignoradas, salta a la siguiente
-					if (ignoredWords.contains(word))
-						continue;
 					// posteos para este documento
 					Map<Integer, Integer> posts = wordPosts.getOrDefault(word, new HashMap());
 					int tf = posts.getOrDefault(iddoc, 0) + 1;
-					// si tiene frecuencia alta en el documento es posible que sea stop word
-					if (tf < 50) {
-						posts.put(iddoc, tf);
-						wordPosts.put(word, posts);
-					} else {
-						// si el largo de la palabra es menor a 2
-						if (word.length() < 2) {
-							wordPosts.remove(word);
-							ignoredWords.add(word);
-						}
-					}
+					posts.put(iddoc, tf);
+					wordPosts.put(word, posts);
 				}
 			});
 
@@ -117,10 +103,11 @@ public class IndexingService {
 			}
 			
 			words[cnt] = v;
-			
-			if (cnt > 1 && cnt++ % chunkSize == 0) {
+			cnt++;
+			if (cnt % chunkSize == 0) {
 				vocabularioDao.createBatch(words);
 				words = new Vocabulario[chunkSize];
+				cnt = 0;
 			}
 		}
 		if(words.length > 0) {
